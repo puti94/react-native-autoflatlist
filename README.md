b9e72affecc87536bbd620aaaa6f400a626e602f
putixie <1059592160@qq.com>
2017-11-15 16:17:12 +0800

    first

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # react-native-autoflatlist
 一个自动上拉下拉提供分页加载的FlatList组件
+
+- 安装:
+   ```
+       npm install react-native-autoflatlist --save
+   ```
+- 使用:
+   ```
+   import AutoFlatList from 'react-native-autoflatlist'
+   ```
+
+   ```
+    /**
+     * 模拟从网络上加载数据
+     * @param page
+     * @returns {Promise}
+     */
+    loadData(page) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                // reject('发生错误'); //模拟发生错误
+                let data = [];
+                for (let i = 0; i < 10; i++) {
+                    data.push(`页数${page},数据源${i}`)
+                }
+
+                //模拟没有更多数据
+                //data.splice(5, 3)
+
+                resolve(data)
+            }, 1000)
+        })
+    }
+
+    renderItem(item, index) {
+        return <Text style={{fontSize: 28, padding: 40, backgroundColor: 'white', marginTop: 1}}>{item}</Text>
+    }
+
+    render() {
+        return (
+            <AutoFlatList
+                style={{flex: 1, marginTop: 64, backgroundColor: '#eee'}}
+                netWork={(page) => this.loadData(page)}
+                loadMoreEnable={true}
+                renderItem={({item, index}) => this.renderItem(item, index)}
+            />
+        );
+    }
+
+```
+
+## Props
+| Prop | Type | Default | Note |
+|---|---|---|---|
+| [FlatList props...](https://facebook.github.io/react-native/docs/FlatList.html) |  |  | SearchInput 组件继承 View 组件的全部属性。
+| style | 同View.style |  | 组件样式, 也就是组件的容器 View 的样式。
+| refreshEnable | bool | true | 是否能下拉刷新
+| loadMoreEnable | bool | false | 是否能上拉加载
+| netWork | func |  | 数据加载方法,会传一个page,需要回传一个Promise,resolve需要传入一个数组
+| emptyButtonTitle | string | '重新加载' | 空视图按钮的文字
+| emptyOnPress | func |  | 默认提供空视图的按钮点击事件，不传则执行onRefresh方法
+| emptyView | element |  | 覆盖原有的空视图
+
+## Events
+| Event Name | Returns | Notes |
+|---|---|---|
+| reLoadData |  | 重新加载数据
---
+++ index.js
@@ -0,0 +1,179 @@
+/**
+ * User: puti.
+ * Time: 2017/11/15 下午1:45.
+ * GitHub:https://github.com/puti94
+ * Email:guoquanxie@foxmail.com
+ */
+
+import React, {Component} from 'react';
+import {
+    View,
+    FlatList,
+    Text,
+    TouchableOpacity,
+    ActivityIndicator
+} from 'react-native';
+import PropTypes from 'prop-types';
+
+export default class AutoFlatList extends Component {
+
+    /**
+     * netWord 是一个回调方法,会将当前page回传,需要返回一个Promise,resolve数据自行处理成array类型
+     * @type {{refreshEnable: shim, loadMoreEnable: shim, netWork: (*), emptyButtonTitle: shim, emptyOnPress: shim}}
+     */
+    static propTypes = {
+        ...FlatList.propTypes,
+        refreshEnable: PropTypes.bool,
+        loadMoreEnable: PropTypes.bool,
+        netWork: PropTypes.func.isRequired,
+        emptyButtonTitle: PropTypes.string,
+        emptyOnPress: PropTypes.func,
+        noMoreDataSize:PropTypes.number,
+        emptyView:PropTypes.element
+    };
+
+    static defaultProps = {
+        ...FlatList.defaultProps,
+        refreshEnable: true,
+        loadMoreEnable: false,
+        noMoreDataSize:10,
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+            refreshing: false,
+            loadMoreing: false,
+            isNoMoreData: false,
+            emptyViewHint: '暂无数据'
+        };
+    }
+
+    componentDidMount() {
+        this.page = 1;
+        this.setState({refreshing: true});
+        this.loadData()
+    }
+
+    /**
+     * 重新刷新数据
+     */
+    reLoadData() {
+        this.setState({data: this.state.data})
+    }
+
+    /**
+     * 返回列表为空的视图
+     * @returns {XML}
+     */
+    renderEmpty() {
+        if (!this.props.emptyView){
+            return this.props.emptyView
+        }
+
+
+        return <View style={{flex: 1, alignItems: 'center', marginTop: 30, justifyContent: 'center'}}>
+
+            <Text style={{fontSize: 18, color: 'black', marginTop: 15}}>{this.state.emptyViewHint}</Text>
+            <TouchableOpacity
+                style={{
+                    marginTop: 20,
+                    backgroundColor: '#00000000',
+                    borderRadius: 5,
+                    borderWidth: 1,
+                    borderColor: 'gray',
+                    height: 40,
+                    width: 120,
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }}
+                onPress={!this.props.emptyOnPress ? () => this.onRefresh() : this.props.emptyOnPress }
+            >
+
+                <Text>{!this.props.emptyButtonTitle ? '重新加载' : this.props.emptyButtonTitle}</Text>
+            </TouchableOpacity>
+        </View>
+    }
+
+    /**
+     * 加载数据
+     */
+    loadData() {
+        this.props.netWork(this.page).then(data => {
+            if (this.page === 1) {
+                this.setState({
+                    data: data,
+                    refreshing: false,
+                    isNoMoreData: data.length < 10
+                })
+            } else {
+                this.setState({
+                    data: this.state.data.concat(data),
+                    isNoMoreData: data.length < 10,
+                    loadMoreing: false
+                })
+            }
+        }).catch((e) => {
+            //加载数据发生错误时
+            if (this.page !== 1) {
+                this.page -= 1;
+                this.setState({loadMoreing: false});
+            } else {
+                this.setState({refreshing: false, emptyViewHint: e});
+            }
+        });
+    }
+
+    /**
+     * 下载刷新时出触发
+     */
+    onRefresh() {
+        this.page = 1;
+        this.setState({refreshing: true, loadMoreing: false});
+        this.loadData()
+    }
+
+    onLoadMore(info) {
+        if (this.state.loadMoreing || this.state.isNoMoreData) return;
+        this.page += 1;
+        this.loadData()
+        this.setState({loadMoreing: true, refreshing: false})
+    }
+
+    renderLoadMoreView() {
+        if (!this.props.loadMoreEnable || this.state.data.length === 0 || !this.state.loadMoreing) return;
+        if (this.state.isNoMoreData) {
+            return <Text style={{alignSelf: 'center'}}>没有更多数据</Text>
+        }
+        if (this.state.loadMoreing) {
+            return <View
+                style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center', flex: 1, height: 50}}>
+                <ActivityIndicator/>
+                <Text style={{marginLeft: 20}}>加载中...</Text>
+            </View>
+        }
+    }
+
+    render() {
+        return (
+            <FlatList
+                {...this.props}
+                data={this.state.data}
+                ListEmptyComponent={this.renderEmpty()}
+                ListFooterComponent={this.renderLoadMoreView()}
+                refreshing={this.state.refreshing}
+                onRefresh={this.props.refreshEnable ? () => {
+                    this.onRefresh()
+                } : null}
+                keyExtractor={(item, index) => index }
+                onEndReachedThreshold={this.props.loadMoreEnable ? 0.1 : null}
+                onEndReached={this.props.loadMoreEnable ? (info) => {
+                    this.onLoadMore(info)
+                } : null}
+
+            />
+        );
+    }
+}
+
